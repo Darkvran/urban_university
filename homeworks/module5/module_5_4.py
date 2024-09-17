ba100c51cf45b7ed5f2c2f6e45440992a89062ef
Darkvran <darkvran2019@gmail.com>
2024-09-17 19:38:55 +0300

    module_5_4.py done

--- homeworks/module5/module_5_4.py
+++ homeworks/module5/module_5_4.py
@@ -1,15 +1,18 @@
 class House:
+    houses_history = []
 
     def __init__(self, name, number_of_floors):
         self.name = name
         self.number_of_floors = number_of_floors
-
+        House.houses_history.append(self.name)
     def __len__(self):
         return self.number_of_floors
 
     def __str__(self):
         return f'Название: {self.name}, кол-во этажей: {self.number_of_floors}'
 
+    def __del__(self):
+        print(f'{self.name} снесён, но он останется в истории')
     def __eq__(self, other):
         if isinstance(other, int) or isinstance(other, House):
             return self.number_of_floors == other.number_of_floors
@@ -53,27 +56,16 @@ class House:
         else:
             print('Такого этажа не существует')
 
-
 h1 = House('ЖК Эльбрус', 10)
+print(House.houses_history)
 h2 = House('ЖК Акация', 20)
+print(House.houses_history)
+h3 = House('ЖК Матрёшки', 20)
+print(House.houses_history)
 
-print(h1)
-print(h2)
+del h2
+del h3
 
-print(h1 == h2)  # __eq__
+print(House.houses_history)
 
-h1 = h1 + 10  # __add__
-print(h1)
-print(h1 == h2)
-
-h1 += 10  # __iadd__
-print(h1)
-
-h2 = 10 + h2  # __radd__
-print(h2)
-
-print(h1 > h2)  # __gt__
-print(h1 >= h2)  # __ge__
-print(h1 < h2)  # __lt__
-print(h1 <= h2)  # __le__
-print(h1 != h2)  # __ne__
+#Объясните пожалуйста, чем плох такой метод? (без args) Не совсем понимаю, как реализовать иначе и для чего.

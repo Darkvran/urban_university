057126dde6f9f12140ec3fa6f98d7a7da1f22a82
Darkvran <darkvran2019@gmail.com>
2024-10-16 02:10:47 +0300

    module_9_6.py done

---
+++ homeworks/module9/module_9_6.py
@@ -0,0 +1,13 @@
+from itertools import combinations
+
+
+def all_variants(text):
+    result = []
+    for i in range(1, len(text) + 1):
+        result += combinations(text, i)
+    for _ in result:
+        yield _
+
+
+for el in all_variants('abc'):
+    print(*el)

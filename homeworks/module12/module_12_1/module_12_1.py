6caa44e2fa5d83c65fde610b44c09ae7f8e33bf2
Darkvran <vladislav.voron.2017@mail.ru>
2024-12-11 17:49:17 +0300

    module_12_1 done

---
+++ homeworks/module12/module_12_1/module_12_1.py
@@ -0,0 +1,25 @@
+from unittest import TestCase, main
+from runner import Runner
+
+class RunnerTest(TestCase):
+    def test_walk(self):
+        runner = Runner('')
+        for _ in range(10):
+            runner.walk()
+        self.assertEqual(runner.distance, 50)
+
+    def test_run(self):
+        runner = Runner('')
+        for _ in range(10):
+            runner.run()
+        self.assertEqual(runner.distance, 100)
+
+    def test_challenge(self):
+        runner1 = Runner("1")
+        runner2 = Runner("2")
+        for _ in range(10):
+            runner1.walk()
+            runner2.run()
+        self.assertNotEqual(runner1.distance, runner2.distance)
+if __name__ == "__main__":
+    main()

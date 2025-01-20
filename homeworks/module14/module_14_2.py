8c0e38753e8950157319bc62a218e7fba4a29d49
Darkvran <vladislav.voron.2017@mail.ru>
2025-01-20 08:09:10 +0300

    New: module_14_2

--- homeworks/module14/module_14_2.py
+++ homeworks/module14/module_14_2.py
@@ -24,10 +24,20 @@ cursor.execute(" CREATE INDEX IF NOT EXISTS idx_email ON Users (email)")
 #
 # for i in range(10):
 #     if i % 3 == 0:
-#         cursor.execute("DELETE FROM Users WHERE username = ?", (f"User{i+1}",))z
-cursor.execute("SELECT username, email, age, balance from Users WHERE age != ?", (60,))
-users = cursor.fetchall()
-for user in users:
-    print(f"Имя: {user[0]} | Почта: {user[1]} | Возраст: {user[2]} | Баланс: {user[3]}")
+#         cursor.execute("DELETE FROM Users WHERE username = ?", (f"User{i+1}",))
+
+#cursor.execute("DELETE FROM Users WHERE id = 6")
+
+# cursor.execute("SELECT username, email, age, balance from Users WHERE age != ?", (60,))
+# users = cursor.fetchall()
+# for user in users:
+#     print(f"Имя: {user[0]} | Почта: {user[1]} | Возраст: {user[2]} | Баланс: {user[3]}")
+
+cursor.execute("SELECT COUNT(*) FROM Users")
+total_us = cursor.fetchone()[0]
+cursor.execute("SELECT SUM(balance) FROM Users")
+total_sum = cursor.fetchone()[0]
+print(total_sum/total_us)
+
 connection.commit()
 connection.close()
